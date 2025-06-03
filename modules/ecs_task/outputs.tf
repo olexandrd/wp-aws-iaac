1c5fafb778841ac96f7d69307ab5b6a565ad2395
Olexandrd <olexandrd@gmail.com>
2025-06-03 22:12:31 +0300

    Add output for ECS service name in task module

--- modules/ecs_task/outputs.tf
+++ modules/ecs_task/outputs.tf
@@ -2,3 +2,8 @@ output "task_definition_arn" {
   description = "ARN of the ECS Task Definition for WordPress"
   value       = aws_ecs_task_definition.wordpress.arn
 }
+
+output "service_name" {
+  description = "Name of the ECS service for WordPress"
+  value       = aws_ecs_service.wordpress_service.name
+}
